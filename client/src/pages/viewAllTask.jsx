de07ee408665bc4463c6e6511b121d85e6d6686e
sumitshinde-84 <shindesumit217@gmail.com>
2023-08-12 20:12:54 +0530

    add logic for view all task

--- client/src/pages/viewAllTask.jsx
+++ client/src/pages/viewAllTask.jsx
@@ -1,5 +1,41 @@
+import  { useEffect, useState } from "react";
+
 const ViewAllTasks = () => {
-    return <></>;
+    const [taskList, setTaskList] = useState([]);
+
+    useEffect(() => {
+        const viewTasks = async () => {
+            try {
+                const res = await fetch('http://localhost:3000/viewTasks', {
+                    method: "GET",
+                    headers: {
+                        "Content-Type": "application/json"
+                    }
+                });
+                const data = await res.json();
+
+                if (data.status === 200) {
+                    setTaskList(data.taskObj);
+                    console.log(data.taskObj);
+                }
+            } catch (err) {
+                console.error(err);
+            }
+        };
+
+        viewTasks();
+    }, []); 
+
+    return (
+        <div>
+            <h1>View All Tasks</h1>
+            <ul>
+                {taskList.map(task => (
+                    <li key={task.id}>{task.title}</li>
+                ))}
+            </ul>
+        </div>
+    );
 };
 
 export default ViewAllTasks;
